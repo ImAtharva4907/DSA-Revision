5475c0a09d1dc09f15c69623b1deb2c58b3cfc20
Atharva chauhan <akashsingh4907@gmail.com>
2025-03-19 01:59:24 +0530

    Binary Tree

---
+++ rightSideView.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "C:\Users\ASUS\Documents\New folder (2)\Dsa Sheet\mylibrary.h"
+using namespace mylib;
+using namespace std;
+
+class Solution {
+    private:
+        void solve(TreeNode* root,vector<int> &arr,int lvl)
+        {
+            if(root==nullptr)
+            {
+                return;
+            }
+    
+            if(arr.size()==lvl)
+            {
+                arr.push_back(root->val);
+      
+            }
+            solve(root->right,arr,lvl+1);
+            solve(root->left,arr,lvl+1);
+        }
+    public:
+        vector<int> rightSideView(TreeNode* root) {
+            vector<int> ans;
+            int l = 0;
+            solve(root,ans,l);
+            return ans;
+        }
+    };
+int main() {
+    
+    return 0;
+}
